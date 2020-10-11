7b73cafa66aa10288a1683c4556c93554c042319
rafaelghesti <66754636+rafaelghesti@users.noreply.github.com>
2020-10-11 14:18:16 -0300

    Adicionar arquivos de projeto.

---
+++ ExceptionsPratics/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExceptionsPratics.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
---
+++ ExceptionsPratics/Program.cs
@@ -0,0 +1,38 @@
+﻿using ExceptionsPratics.Entities;
+using ExceptionsPratics.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace ExceptionsPratics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("## Enter account data ##");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holer = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Whitdraw limit: ");
+            double whitdrawLimit = double.Parse(Console.ReadLine());
+            
+            Account acc = new Account(number, holer, balance, whitdrawLimit);
+
+            Console.Write("\nEnter amount for whitdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine($"New balance: ${acc.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Whitdraw error: {e.Message}");
+            }
+
+        }
+    }
+}
